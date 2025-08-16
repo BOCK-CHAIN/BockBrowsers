cd6a5612fd4bd06d4d77cfae0bc589002cba1206
Siona-Raj <sionaraj1@gmail.com>
2025-08-16 17:13:34 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  navigateTo: (page) => ipcRenderer.send('navigate', page),
-  openNewTab: () => ipcRenderer.send('open-new-tab'),
-  openNewWindow: () => ipcRenderer.send('open-new-window'),
-  openIncognito: () => ipcRenderer.send('open-incognito')
-});
